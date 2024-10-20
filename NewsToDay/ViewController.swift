28b476348820ffbf34109c1c5215aea7e15bf323
vvp-off <reservvp@gmail.com>
2024-10-20 23:05:07 +0300

    Initial Commit

---
+++ NewsToDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsToDay
+//
+//  Created by vp.off on 20.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
