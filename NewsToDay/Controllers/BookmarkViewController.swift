5f2577aea8a8e5d28278d9f1e101771dca128236
vvp-off <reservvp@gmail.com>
2024-10-22 15:57:47 +0300

    added and customized TabBar

---
+++ NewsToDay/Controllers/BookmarkViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkViewController.swift
+//  NewsToDay
+//
+//  Created by vp.off on 22.10.2024.
+//
+
+import UIKit
+
+class BookmarkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+}
---
+++ NewsToDay/Controllers/UserViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserViewController.swift
+//  NewsToDay
+//
+//  Created by vp.off on 22.10.2024.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+
+}
